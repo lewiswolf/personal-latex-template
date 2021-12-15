9822586d589165bbaa4ea602997ea8ea41f01d0d
lewiswolf <lewiswolstanholme@gmail.com>
2021-12-15 22:13:31 +0000

    update to bbt postscript to format video citations correctly

--- zotero/bbt-postscript.js
+++ zotero/bbt-postscript.js
@@ -4,19 +4,53 @@ For information on installing these scripts, see here =>
 	https://retorque.re/zotero-better-bibtex/exporting/scripting/
 */
 
-// add url and urldate to webpage citations
-if (Translator.BetterBibTeX && item.itemType === 'webpage') {
-	reference.remove('howpublished')
-	if (item.url) {
-		reference.add({
-			name: 'url',
-			bibtex: `{${reference.enc_verbatim({ value: item.url })}}`,
-		})
-	}
-	if (item.accessDate) {
-		reference.add({
-			name: 'urldate',
-			bibtex: `{${item.accessDate.replace(/\s*T?\d+:\d+:\d+.*/, '')}}`,
-		})
+if (Translator.BetterBibTeX) {
+	switch (item.itemType) {
+		// add url and urldate to webpage citations
+		case 'webpage':
+			reference.remove('howpublished')
+			if (item.url) {
+				reference.add({
+					name: 'url',
+					bibtex: `{${reference.enc_verbatim({ value: item.url })}}`,
+				})
+			}
+			if (item.accessDate) {
+				reference.add({
+					name: 'urldate',
+					bibtex: `{${item.accessDate.replace(/\s*T?\d+:\d+:\d+.*/, '')}}`,
+				})
+			}
+		// format a video recording
+		case 'videoRecording':
+			// merge producer and director fields
+			for (const creator of item.creators) {
+				creator.creatorType = creator.creatorType === 'director'
+					|| creator.creatorType === 'producer'
+					? 'director' // not sure why, but author did not work here
+					: content.creatorType
+			}
+			reference.addCreators()
+			// add format
+			if (item.medium) {
+				reference.add({
+					name: 'type',
+					bibtex: `{${item.medium}}`,
+				})
+			}
+			// add url and urldate
+			if (item.url && item.accessDate) {
+				reference.add({
+					name: 'url',
+					bibtex: `{${reference.enc_verbatim({ value: item.url })}}`,
+				})
+				reference.add({
+					name: 'urldate',
+					bibtex: `{${item.accessDate.replace(/\s*T?\d+:\d+:\d+.*/, '')}}`,
+				})
+			}
+		// do nothing if default case
+		default:
+			break
 	}
 }
